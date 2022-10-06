fdbe62e86d6698185c6e88e302701c14c702059e
thmcheung <tingho.marcus.cheung@outlook.com>
2022-10-05 23:10:01 -0400

    Commit

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 0; i <= 10; i++){
+            String temp = String.valueOf(10-i);
+            System.out.println("Current count: " + temp);
+        }
 
 
     }
@@ -141,9 +147,16 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
-        // Fill in the rest of the body here
-
+        boolean flag = true;
+        for (int i = 0; i < to_split.length(); i++){
+            if (flag){
+                ret.append(to_split.charAt(i));
+                flag = false;
+            }
+            if (to_split.charAt(i) == (' ')){
+                flag = true;
+            }
+        }
         return ret.toString();
     }
 
@@ -170,7 +183,11 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        for (int i = 0; i < arr.length; i ++){
+            if (i % 2 == 1){
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
